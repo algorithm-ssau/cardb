3ecf2b0db53932c044271abf140c205269499902
AleksandrShevelev <61016378+AleksandrShevelev@users.noreply.github.com>
2020-03-30 21:06:40 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -34,3 +34,17 @@ window.onload = function() {
     $("body").removeClass("stop-scrolling");
   };
 };
+
+var background_image_parallax = function ($object, multiplier) {
+  multiplier = typeof multiplier !== "undefined" ? multiplier : 0.5;
+  multiplier = 1 - multiplier;
+  var $doc = $(document);
+  $object.css({ "background-attatchment": "fixed" });
+  $(window).scroll(function () {
+    var from_top = $doc.scrollTop(),
+      bg_css = "0px " + multiplier * from_top + "px";
+    $object.css({ "background-position": bg_css });
+  });
+};
+background_image_parallax($(".box2"));
+background_image_parallax($(".box3"), 0.25);
